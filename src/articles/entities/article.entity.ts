edf65d7f876892845d780bd949d52cddd58a66f2
Arsenii <origingoy@gmail.com>
2023-07-27 17:19:19 +0300

    added constructor

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Article } from "@prisma/client";
+import { UserEntity } from "src/users/entities/user.entity";
 
 
 export class ArticleEntity implements Article {
@@ -26,4 +27,14 @@ export class ArticleEntity implements Article {
 
     @ApiProperty({ required: false, nullable: true })
     authorId: number | null;
+
+    @ApiProperty({ required: false, type: UserEntity })
+    author?: UserEntity;
+
+    constructor({ author, ...data }: Partial<ArticleEntity>) {
+        Object.assign(this, data);
+        if (author) {
+            this.author = new UserEntity(author);
+        }
+    }
 }
