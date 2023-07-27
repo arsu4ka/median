6f4ccb023a834b8b7d2f9ac1867d703758efd7dd
Arsenii <origingoy@gmail.com>
2023-07-27 14:49:41 +0300

    created create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    name: string;
+
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    @ApiProperty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @ApiProperty()
+    password: string;
+}
