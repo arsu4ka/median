52fc7fcf8c9ae0f6f8814918e1ccca7a1995c842
Arsenii <origingoy@gmail.com>
2023-07-28 13:15:13 +0300

    added endpoint for token generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { AuthEntity } from './entity/auth.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
+@ApiTags("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post("login")
+  @ApiOkResponse({ type: AuthEntity })
+  async login(@Body() { email, password }: LoginDto) {
+    const authEntity = await this.authService.login(email, password);
+    return authEntity;
+  }
 }
