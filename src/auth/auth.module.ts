6eb071df555f0f6756fc5a88a1304ec4e3915dc6
Arsenii <origingoy@gmail.com>
2023-07-28 13:45:44 +0300

    removed unnecessary code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,8 @@ import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-
-function generateRandomPassword(length: number): string {
-  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let password = '';
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
-  }
-
-  return password;
-}
+import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
@@ -23,11 +13,12 @@ function generateRandomPassword(length: number): string {
     PrismaModule,
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET?? generateRandomPassword(16),
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '48h' }
-    })
+    }),
+    UsersModule
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
