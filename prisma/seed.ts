f6d08af9153f6d20a8361387e175a039456e7bd2
Arsenii WSL <origingoy@gmail.com>
2024-04-27 14:32:30 +0300

    formatted seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,89 +1,89 @@
-import { PrismaClient } from "@prisma/client";
-import * as bcrypt from "bcrypt";
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const roundsOfHashing = 10;
 
 const prisma = new PrismaClient();
 
 async function main() {
-    const user1HashedPassword = await bcrypt.hash("password-sabin", roundsOfHashing);
-    const user2HashedPassword = await bcrypt.hash("password-alex", roundsOfHashing);
+  const user1HashedPassword = await bcrypt.hash('password-sabin', roundsOfHashing);
+  const user2HashedPassword = await bcrypt.hash('password-alex', roundsOfHashing);
 
-    const user1 = await prisma.user.upsert({
-        where: { email: 'sabin@adams.com' },
-        update: {
-          password: user1HashedPassword
-        },
-        create: {
-        email: 'sabin@adams.com',
-        name: 'Sabin Adams',
-        password: user1HashedPassword,
-        },
-    });
+  const user1 = await prisma.user.upsert({
+    where: { email: 'sabin@adams.com' },
+    update: {
+      password: user1HashedPassword,
+    },
+    create: {
+      email: 'sabin@adams.com',
+      name: 'Sabin Adams',
+      password: user1HashedPassword,
+    },
+  });
 
-    const user2 = await prisma.user.upsert({
-        where: { email: 'alex@ruheni.com' },
-        update: {
-          password: user2HashedPassword
-        },
-        create: {
-        email: 'alex@ruheni.com',
-        name: 'Alex Ruheni',
-        password: 'password-alex',
-        },
-    });
+  const user2 = await prisma.user.upsert({
+    where: { email: 'alex@ruheni.com' },
+    update: {
+      password: user2HashedPassword,
+    },
+    create: {
+      email: 'alex@ruheni.com',
+      name: 'Alex Ruheni',
+      password: 'password-alex',
+    },
+  });
 
-    const post1 = await prisma.article.upsert({
-        where: { title: 'Prisma Adds Support for MongoDB' },
-        update: {
-          authorId: user1.id
-        },
-        create: {
-          title: 'Prisma Adds Support for MongoDB',
-          body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
-          description:
-            "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
-          published: false,
-          authorId: user1.id
-        },
-    });
-    
-    const post2 = await prisma.article.upsert({
-        where: { title: "What's new in Prisma? (Q1/22)" },
-        update: {
-          authorId: user2.id
-        },
-        create: {
-          title: "What's new in Prisma? (Q1/22)",
-          body: 'Our engineers have been working hard, issuing new releases with many improvements...',
-          description:
-            'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
-          published: true,
-          authorId: user2.id
-        },
-    });
+  const post1 = await prisma.article.upsert({
+    where: { title: 'Prisma Adds Support for MongoDB' },
+    update: {
+      authorId: user1.id,
+    },
+    create: {
+      title: 'Prisma Adds Support for MongoDB',
+      body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
+      description:
+        "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
+      published: false,
+      authorId: user1.id,
+    },
+  });
 
-    const post3 = await prisma.article.upsert({
-        where: { title: 'Prisma Client Just Became a Lot More Flexible' },
-        update: {},
-        create: {
-          title: 'Prisma Client Just Became a Lot More Flexible',
-          body: 'Prisma Client extensions provide a powerful new way to add functionality to Prisma in a type-safe manner...',
-          description:
-            'This article will explore various ways you can use Prisma Client extensions to add custom functionality to Prisma Client..',
-          published: true,
-        },
-    });
+  const post2 = await prisma.article.upsert({
+    where: { title: "What's new in Prisma? (Q1/22)" },
+    update: {
+      authorId: user2.id,
+    },
+    create: {
+      title: "What's new in Prisma? (Q1/22)",
+      body: 'Our engineers have been working hard, issuing new releases with many improvements...',
+      description:
+        'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
+      published: true,
+      authorId: user2.id,
+    },
+  });
 
-    console.log({user1, user2});
-    console.log({post1, post2, post3});
+  const post3 = await prisma.article.upsert({
+    where: { title: 'Prisma Client Just Became a Lot More Flexible' },
+    update: {},
+    create: {
+      title: 'Prisma Client Just Became a Lot More Flexible',
+      body: 'Prisma Client extensions provide a powerful new way to add functionality to Prisma in a type-safe manner...',
+      description:
+        'This article will explore various ways you can use Prisma Client extensions to add custom functionality to Prisma Client..',
+      published: true,
+    },
+  });
+
+  console.log({ user1, user2 });
+  console.log({ post1, post2, post3 });
 }
 
 main()
-    .catch(e => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
